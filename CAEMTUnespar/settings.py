f6cefc921b2ad5a64c23f9993d4b0b6ab021a840
thisisrenan <joserenandasilva08@gmail.com>
2023-10-17 07:33:41 -0300

    Iniciando telas

--- CAEMTUnespar/settings.py
+++ CAEMTUnespar/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'CAEMTUnespar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'core/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
