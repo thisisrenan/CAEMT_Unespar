e6e7210ce66bf01a7eb42a315700ebd21829512d
thisisrenan <joserenanadsilva08@gmail.com>
2024-01-09 20:35:22 -0300

    Criando form orientador

--- core/forms.py
+++ core/forms.py
@@ -2,8 +2,19 @@ from django import forms
 from .models.users import *
 
 class OrientadorForm(forms.ModelForm):
+    password_confirm = forms.CharField(widget=forms.PasswordInput, label='Confirme a Senha')
+
     class Meta:
         model = Orientador
         fields = '__all__'
         exclude = ['role', 'user_permissions', 'groups', 'is_staff', 'is_superuser', 'last_login', 'is_active']
+        fields = ['username', 'password', 'password_confirm', 'first_name', 'last_name', 'email', 'data_de_nascimento', 'telefone', 'image', 'data_final', 'role']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("As senhas não coincidem. Por favor, tente novamente.")
 
